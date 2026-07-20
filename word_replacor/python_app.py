cf438f441cc6597986f1ab44d0d06c69b94a5752
Hassan Abid 15 <bsse23015@itu.edu.pk>
2026-07-20 13:16:16 +0500

    Word replacement funtion implemented

---
+++ word_replacor/python_app.py
@@ -0,0 +1,10 @@
+def word_replace():
+    sentence="Meka ladla meow gop gop gop gop gop gop"
+    word_to_replace=input("Enter the word u wanna replace:")
+    word_to_replace_with=input("Enter the word u wanna replace with:")
+    replaced_sentence=sentence.replace(word_to_replace,word_to_replace_with)
+    print(replaced_sentence)
+
+word_replace()
+
+    
